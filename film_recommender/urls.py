798be3d48450f8384f31f3f226f0eae1aadf6e86
Tethyra <15905935660@163.com>
2025-11-09 10:23:04 +0800

    1

--- film_recommender/urls.py
+++ film_recommender/urls.py
@@ -6,11 +6,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf import settings  # 添加这行
+from django.conf.urls.static import static  # 添加这行
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),  # 包含main应用的URL
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
 
 if settings.DEBUG:
     # 开发环境下提供静态文件服务
